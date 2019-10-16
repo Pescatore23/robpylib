0cd247a7f620e2d93820845a7ee8944729f77fcf
Fischer, Robert <robert.fischer@empa.ch>
2019-10-16 14:37:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='RobPyLib',
       version='1.0',
-      packages=['RobPyLib', 'RobPyLib.CommonFunctions', 'RobPyLib.NetworkGeneration', 'RobPyLib.NetworkGeneration.JeffGostick'],
+      packages=['RobPyLib'],
 	  install_requires=['numpy','os','skimage','scipy', 'csv'],
       )
