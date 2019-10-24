c7588764b5c707c4c843ee480a1fa58b462d0a78
Robert <robert.fischer@empa.ch>
2019-10-24 16:51:46 +0200

    TOMCAT list update

--- robpylib/TOMCAT/INFO.py
+++ robpylib/TOMCAT/INFO.py
@@ -8,4 +8,9 @@ Created on Thu Oct 24 12:03:40 2019
 
 # samples with no processing errors
 good_samples = ['T3_100_7_III',
-                'T3_100_100_III']
+                'T3_100_100_III',
+				'T3_300_5',
+				'T3_025_9']
+				
+questionable_sample = ['T3_300_9_III',       #wet with acetone
+						'T3_100_4_III']     #wet with acetone
